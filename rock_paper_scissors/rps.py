7ecf4d39fea95a139f4132c0446f1132a6234b6c
newbi462 <isthisemailfree@gmail.com>
2020-04-16 11:21:25 -0400

    Rock Paper Scissors | In progress

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,26 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    # what are the choices
+    plays = ['rock', 'paper', 'scissors']
+    awnser = []
+    round_count = 0
+    #iterate over the choices that could be made
+    if n == 0:
+        return [ []]
+    else:
+        for x in plays:
+            print("For x in plays ran")
+            if True:
+                awnser.append([])
+                awnser[round_count].append(x)
+                if len(awnser[round_count]) == n:
+                    round_count += 1
+                    print("next set")
+                print("if True Ran")
+        # terminate and reducse as the rounds take place
+        print(awnser)
+        return awnser
 
 
 if __name__ == "__main__":
@@ -12,3 +31,7 @@ if __name__ == "__main__":
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
+
+
+test = 1
+print(rock_paper_scissors( test))
